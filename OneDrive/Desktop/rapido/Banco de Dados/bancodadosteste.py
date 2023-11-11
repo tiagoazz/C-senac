cf4418f5a68d67c1e8d3db407d29cdc3e3392379
João Pedro Ferrari <70978145+pedrooferrari@users.noreply.github.com>
2023-11-11 12:42:33 -0300

    identacao
    identacao

--- OneDrive/Desktop/rapido/Banco de Dados/bancodadosteste.py
+++ OneDrive/Desktop/rapido/Banco de Dados/bancodadosteste.py
@@ -1,8 +1,12 @@
 import mysql.connector
 from datetime import datetime
 
+# IMPORTANDO AS BIBLIOTECAS NECESSARIAS
+
 conectar = mysql.connector.connect(host='localhost', user='root', password='', database='empresa')
-cursor = conectar.cursor()
+cursor = conectar.cursor() 
+
+# FAZENDO A CONEXAO AO SERVIDOR MYSQL (XAMPP SERVER)
 
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS funcionarios (
@@ -12,20 +16,26 @@ cursor.execute('''
         idade INT          
     )          
 ''')
+# CRIANDO A TABELA "funcionarios" | TAMBEM PODERIA SER CRIADA ANTES DENTRO DO BANCO
 
 def calcular_idade(data_nascimento):
     hoje = datetime.now()
     nascimento = datetime.strptime(data_nascimento, '%Y-%m-%d')
     idade = hoje.year - nascimento.year - ((hoje.month, hoje.day) < (nascimento.month, nascimento.day))
 
+# ALGORITMO PARA CALCULO DE IDADE BASEADO NO NASCIMENTO INSERIDO 
+
 nome = input("Digite o nome do funcionário: ")
 nascimento = input("Digite a data de nascimento (formato AAAA-MM-DD): ")
 
 idade = calcular_idade(nascimento)
+# ALGORTIMO QUE ARMAZENA OS VALORES DE nome, nascimento e idade (QUE SERAO ACRESCENTADOS À TABELA)
 
 cursor.execute("INSERT INTO funcionarios (nome, nascimento, idade) VALUES (%s, %s, %s)", (nome, nascimento, idade, ))
+# INSERE NA TABELA
 conectar.commit()
-
+# FAZ AS MUDANCAS
 print("Dados inseridos com sucesso!")
 
 conectar.close()
+# FECHA A CONEXAO COM O BANCO DE DADOS
